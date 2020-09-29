0fb875c84a0ec636cde9ec298cc04cd80e224738
Hendrik Mennen <hmennen@vhdplus.com>
2020-09-29 16:02:58 +0200

    .,.

--- NIOSTest/Software/NIOSDuino/NIOSDuino.cpp
+++ NIOSTest/Software/NIOSDuino/NIOSDuino.cpp
@@ -7,7 +7,7 @@ void setup() {
     Serial.begin(9600); //Set Baudrate with "New Processor" test
 }
 
-void loop() {   
+void loop() {
     Serial.println("Hello World");
     delay(1000);
     Serial.println("test");
